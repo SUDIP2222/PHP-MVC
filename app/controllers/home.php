7fcea4359b05603c11e0dec003d7f12a14fea02d
sudip2222 <sudipdiu@gmail.com>
2015-11-28 03:03:38 +0600

    ORM add

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,17 +1,37 @@
 <?php
 
-
      class Home extends Controller {
+         /*
+         protected $user;
+
+         public function __construct(){
+             $this->user=$this->model('User');
+         }  */
 
          public function index($name=""){
 
              // echo $name.$other;
-             $user= $this->model('User');
-             $user->name=$name;
+             /*
+             $user= $this->user;
+             $user->name=$name; */
              //echo $user->name;
 
              $this->view('home/index',['name'=>$user->name]);
 
+             //User::find(1);
          }
 
+         public function create($username='',$email=''){
+              /*
+              $this->user->create([
+                  'username'=>$username,
+                  'email'=>$email
+              ]); */
+
+             User::create([
+                 'username'=>$username,
+                 'email'=>$email
+             ]);
+
+         }
      }
